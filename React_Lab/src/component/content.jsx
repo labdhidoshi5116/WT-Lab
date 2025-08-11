82c20220d2e04305c75cb4da3452f132a5dac23e
Labdhi Doshi <labdhidoshi5116@gmail.com>
2025-08-11 09:59:01 +0530

    new

---
+++ React_Lab/src/component/content.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Content() {
+  return (
+    <>
+      <div className='Hero'>
+      <div className="Content">Content</div>
+      <div className='Main'>Main Content</div>
+      </div>
+    </>
+  )
+}
