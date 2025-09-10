5fe4d6ac9d5cefa45700006e5e292c7ad8568874
Labdhi Doshi <labdhidoshi5116@gmail.com>
2025-09-10 08:05:17 +0530

    new

--- react_lab/src/App4.jsx
+++ react_lab/src/App4.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 // import Navbar from './components/Navbar';
 // import Footer from './components/Footer';
-import Home from './Home';
-import About from './About';
-import Services from './Services';
-import Contact from './Contact';
-import Layout from './Layout';
+import Home from './components/Home';
+import About from './components/About';
+import Services from './components/Services';
+import Contact from './components/Contact';
+import Layout from './components/Layout';
 
 function App4() {
   return (
